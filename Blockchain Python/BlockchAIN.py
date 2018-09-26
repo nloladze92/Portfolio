ce8798da27abe69dce4e5a68518391130234b52b
ttmohni <30649522+ttmohni@users.noreply.github.com>
2018-09-26 04:52:34 -0500

    updated the blockchain project 9/26/2018, still need to add get_chain and is_valid functions, hopefully over the course of this week

--- Blockchain Python/BlockchAIN.py
+++ Blockchain Python/BlockchAIN.py
@@ -29,20 +29,69 @@ class Blockchain:
         def get_previous_block(self):
             return self.chain[-1]
         
+        #the square root of the of an incremented number subtracted from the previous number squared as well
+        #encoded and output as a hexdigest must have four leading zeros in order to mine a block
         def proof_of_work(self, previous_proof):
             new_proof = 1
             check_proof = False
             while check_proof is False:
-                hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest(
-                        if hash_operation[:4] == '0000':
-                            check_proof = True
-                        else: 
-                            new_proof += 1
+                hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+                if hash_operation[:4] == '0000':
+                    check_proof = True
+                else: 
+                    new_proof += 1
                             
         def hash(self, block):
             encoded_block = json.dumps(block, sort_keys = True).encode()
-            return hashlib.sha256(encoded_lbock).hexdigest()
+            return hashlib.sha256(encoded_block).hexdigest()
         
-# part 2 - mining our blockchain
+        #to iterate through the chain array and find if the hash operations starts with four leading zeros
+        #and if the hash of the previous proof matches the proof of the following block
+        def is_chain_valid(self, chain):
+            previous_block = chain[0]
+            block_index = 1
+            while block_index < len(chain):
+                block = chain[block_index]
+                if block['previous_hash'] != self.hash(previous_block):
+                    return False
+                previous_proof = previous_block['proof']
+                proof = block['proof']
+                hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+                if hash_operation[:4] != '0000':
+                    return False
+                previous_block = block
+                block_index += 1
+            return True
+        
+        # part 2 - mining our blockchain
+        #creating a web app
+            app = Flask(__name__)
+            
+            #creating a blockchain
+            
+            blockchain = Blockchain()
+        @app.route('/mine_block', methods = ['GET'])
+        def mine_block():
+            previous_block = blockchain.get_previous_block()
+            previous_proof = previous_block['proof']
+            proof = blockchain.proof_of_work(previous_proof)
+            previous_hash = blockchain.hash(previous_block)
+            block = blockchain.create_block(proof, previous_hash)
+            response = {'message': "Congratulations, you just mined a block!",
+                        'index': block['index'],
+                        'timestamp': block['timestamp'],
+                        'proof': block['proof'],
+                        'previous_hash': block['previous_hash']}
+            return jsonify(response), 200
+            
+            
+            #running the app
+            app.run(host='0.0.0.0', port = '5000')
+            
+            
+            
+            
+            
+            
             
         
