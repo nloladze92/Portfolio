cc9c31830b19dd259158ce8caa8b68b07a1f3e65
nloladze92 <nik.loladze@gmail.com>
2018-11-30 00:59:51 -0600

    updated contracts

--- 3. Python Blockchain Token Protocol/Coin_Protocol.py
+++ 3. Python Blockchain Token Protocol/Coin_Protocol.py
@@ -20,13 +20,13 @@ from urllib.parse import urlparse
 #pip install requests==2.18.4
 
 class Blockchain:
-    
+
     def __init__(self):
         self.chain = []
         self.transactions = []
         self.create_block(proof = 1, previous_hash = '0')
         self.nodes = set()
-        
+
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
@@ -36,10 +36,10 @@ class Blockchain:
         self.transactions = []
         self.chain.append(block)
         return block
-        
+
     def get_previous_block(self):
         return self.chain[-1]
-        
+
     #the square root of the of an incremented number subtracted from the previous number squared as well
     #encoded and output as a hexdigest must have four leading zeros in order to mine a block
     def proof_of_work(self, previous_proof):
@@ -49,14 +49,14 @@ class Blockchain:
             hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
-            else: 
+            else:
                 new_proof += 1
             return new_proof
-                
+
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
-        
+
     #to iterate through the chain array and find if the hash operations starts with four leading zeros
     #and if the hash of the previous proof matches the proof of the following block
     def is_chain_valid(self, chain):
@@ -74,18 +74,18 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
-    
-    def add_transaction(self, sender, receiver, amount): 
+
+    def add_transaction(self, sender, receiver, amount):
         self.transactions.append({'sender': sender,
                                   'receiver': receiver,
                                   'amount': amount})
         previous_block = self.get_previous_block()
         return previous_block['index'] + 1
-        
+
     def add_node(self, address):
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
-           
+
     def replace_chain(self):
         network = self.nodes
         longest_chain = None
@@ -102,17 +102,17 @@ class Blockchain:
             self.chain = longest_chain
             return True
         return False
-        
-        
+
+
         # part 2 - mining our blockchain
         #creating a web app
 app = Flask(__name__)
 
 #creating an address for the node on Port
 node_address = str(uuid4()).replace('-', '')
-            
+
 #creating a blockchain
-            
+
 blockchain = Blockchain()
 
 @app.route('/mine_block', methods = ['GET'])
@@ -130,11 +130,11 @@ def mine_block():
                 'previous_hash': block['previous_hash'],
                 'transactions': block['transactions']}
     return jsonify(response), 200
-        
+
 #getting the full blockchain
 @app.route('/get_chain', methods = ['GET'])
 def get_chain():
-    response = {'chain': blockchain.chain, 
+    response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
@@ -143,9 +143,9 @@ def is_valid():
     is_valid = blockchain.is_chain_valid(blockchain.chain)
     if is_valid:
         response = {'message': 'The blockchain is valid.'}
-    else: 
+    else:
         response = {'message': 'There is a problem.'}
-    return jsonify(response), 200   
+    return jsonify(response), 200
 
 #adding a new transaction to the blockchain
 @app.route('/add_transaction', methods = ['POST'])
@@ -177,21 +177,13 @@ def connect_node():
 def replace_chain():
     is_chain_replaced = blockchain.replace_chain()
     if is_chain_replaced:
-        response = {'message': 'The node had different chains so the chain was replaced b the longest one.',
+        response = {'message': 'The node had different chains so the chain was replaced by the longest one.',
                     'new_chain': blockchain.chain}
-    else: 
+    else:
         response = {'message': 'All good. The chain is the largest one.',
                     'actual_chain': blockchain.chain}
-    return jsonify(response), 200   
-    
+    return jsonify(response), 200
+
 
 #running the app
 app.run(host='0.0.0.0', port = 5000)
-            
-            
-            
-            
-            
-            
-            
-        
