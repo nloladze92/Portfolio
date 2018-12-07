20c785161003ae0930f283c58f9e0706b458e9f0
nloladze92 <nik.loladze@gmail.com>
2018-12-07 17:52:18 -0600

    video list react

--- Contracts/React/src/index.js
+++ Contracts/React/src/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCVPbSk3sFNKAkvTIbEaZ3bgACAG0pKy6s';
 
 
@@ -10,10 +11,16 @@ class App extends Component {
     constructor(props) {
       super(props);
 
-      this.state = { videos: [] };
+      this.state = {
+      videos: [],
+      selectedVideo: null
+     };
+
       YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-        this.setState({ videos });
-        //this.setState({ videos: videos })
+        this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+         });
       });
     }
 
@@ -21,6 +28,10 @@ class App extends Component {
     return (
     <div>
     <SearchBar />
+    <VideoDetail video = {this.state.selectedVideo} />
+    <VideoList
+    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+    videos = {this.state.videos} />
     </div>
     );
   }
