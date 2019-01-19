7d3e3e026f59c88743af3cc69f8a43b83ce4393d
nloladze92 <nik.loladze@gmail.com>
2019-01-18 21:20:26 -0600

    updated jan late end

--- Golang/playing_cards/main.go
+++ Golang/playing_cards/main.go
@@ -1,12 +1,18 @@
 package main
 
 func main() {
+
+	// TESTING OF VARIOUS FUNCTIONS
 	// cards := newDeck()
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// remainingCards.print()
 	// cards := newDeck()
 	// cards.saveToFile("my_cards")
-	cards := newDeckFromFile("my_cars")
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
