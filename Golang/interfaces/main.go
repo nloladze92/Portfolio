75cbc950eeb8113e3b4807fb30a8c84c7b595694
ttmohni <30649522+ttmohni@users.noreply.github.com>
2019-01-30 15:37:05 -0600

    markdown golang

--- Golang/interfaces/main.go
+++ Golang/interfaces/main.go
@@ -2,20 +2,33 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	printGreeting(eb)
+	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+/* func printGreeting(eb englishBot) {
 	fmt.Println(eb.getGreeting())
 }
 
 func printGreeting(sb spanishBot) {
 	fmt.Println(sb.getGreeting())
 }
+*/
 
 func (englishBot) getGreeting() string {
 	// customn logic for generating english greeting
