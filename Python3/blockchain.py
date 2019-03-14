9d4d36feb1594705690623d9828f75f274606054
ttmohni <30649522+ttmohni@users.noreply.github.com>
2019-03-14 04:55:20 -0500

    python3

--- Python3/blockchain.py
+++ Python3/blockchain.py
@@ -1,3 +1,4 @@
+import functools
 MINING_REWARD = 10
 
 genesis_block={
@@ -17,15 +18,14 @@ def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    #calculate the total amount of coins sent
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
+    #amount_sent = 0
+    #for tx in tx_sender:
+    #    if len(tx) > 0:
+     #       amount_sent += tx[0]
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_received = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
     return amount_received - amount_sent
 
 def get_last_blockchain_value():
@@ -170,7 +170,7 @@ while waiting_for_input:
         print_blockchain_elements()
         print('Invalid blockchain!')
         break
-    print(get_balance('Nik'))
+    print('Balance of {}: {:6.2f}'.format('Nik', get_balance('Nik')))
 else:
     print('User left!')
 
--- Python3/data_structure_assignment.py
+++ Python3/data_structure_assignment.py
@@ -13,12 +13,17 @@ persons = [
 ]
 
 person_names = [person['name'] for person in persons]
-print(persons)
-print(person_names)
+#print(persons)
+##print(person_names)
 
 are_older = all([person['age'] > 20 for person in persons])
-print(are_older)
+#print(are_older)
 
 copied_persons = persons[:]
 copied_persons[0]['name'] = 'Maximilian'
-print(copied_persons)
+#print(copied_persons)
+
+def unlimited_arguments(*args):
+    for arguments in args:
+        print(arguments)
+unlimited_arguments(1, 2, 3, 4)
